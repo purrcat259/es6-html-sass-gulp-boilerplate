1fd0bf2147480d16606ee1eaa85c10833e811213
Simon AM <purrcat259@users.noreply.github.com>
2017-10-16 14:40:05 +0200

    Added SASS to build steps

--- gulp/build.js
+++ gulp/build.js
@@ -2,7 +2,7 @@ import gulp from 'gulp';
 import runSequence from 'run-sequence';
 
 gulp.task('build_dist', (callback) => {
-    runSequence('clean', ['build_js_dist', 'build_html_dist'], callback)
+    runSequence('clean', ['build_js_dist', 'build_sass_dist', 'build_html_dist'], callback)
 });
 
-gulp.task('build', ['build_js', 'build_html']);
+gulp.task('build', ['build_js', 'build_sass', 'build_html']);
